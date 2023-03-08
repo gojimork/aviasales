7f34a275552cf1e9739cd866b4ff165b9abfbb66
gojimork <crusejohen@gmail.com>
2023-03-08 14:23:37 +0300

    add redux thunk, add redux devTools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/app";
-import reducer from "./reducer";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app';
+import reducer from './reducer';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-const store = createStore(reducer);
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const loggerMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+  console.log('Middleware', store.getState());
+  return result;
+};
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk)));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />

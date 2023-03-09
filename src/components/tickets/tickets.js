c8524ab404f2bbb4c6ffd567c84a2c47d7539587
gojimork <crusejohen@gmail.com>
2023-03-09 10:41:09 +0300

    get tickets arr grom server and put to store state

--- src/components/tickets/tickets.js
+++ src/components/tickets/tickets.js
@@ -1,170 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { connect } from 'react-redux';
 import s7Logo from '../../assets/s7-logo.svg';
 import classes from './tickets.module.scss';
-const Tickets = () => {
+const Tickets = ({ tickets, loadTickets }) => {
+  useEffect(() => {
+    loadTickets();
+  }, [loadTickets]);
   return (
     <React.Fragment>
       <ul className={classes['tickets-list']}>
-        <li className={classes.ticket}>
-          <div className={classes['ticket__header']}>
-            <span className={classes['ticket__price']}>13 400</span>
-            <div className={classes['ticket__logo']}>
-              <img src={s7Logo} alt="logo" />
-            </div>
-          </div>
-          <div className={classes['ticket__flight']}>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>MOW – HKT</span>
-                <span className={classes['info__value']}>10:45 – 08:00</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>MOW – HKT</span>
-                <span className={classes['info__value']}>11:20 – 00:50</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>21ч 15м</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>13ч 30м</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>2 пересадки</span>
-                <span className={classes['info__value']}>HKG, JNB</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>1 пересадка</span>
-                <span className={classes['info__value']}>HKG</span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li className={classes.ticket}>
-          <div className={classes['ticket__header']}>
-            <span className={classes['ticket__price']}>13 400</span>
-            <div className={classes['ticket__logo']}>
-              <img src={s7Logo} alt="logo" />
-            </div>
-          </div>
-          <div className={classes['ticket__flight']}>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>MOW – HKT</span>
-                <span className={classes['info__value']}>10:45 – 08:00</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>MOW – HKT</span>
-                <span className={classes['info__value']}>11:20 – 00:50</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>21ч 15м</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>13ч 30м</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>2 пересадки</span>
-                <span className={classes['info__value']}>HKG, JNB</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>1 пересадка</span>
-                <span className={classes['info__value']}>HKG</span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li className={classes.ticket}>
-          <div className={classes['ticket__header']}>
-            <span className={classes['ticket__price']}>13 400</span>
-            <div className={classes['ticket__logo']}>
-              <img src={s7Logo} alt="logo" />
-            </div>
-          </div>
-          <div className={classes['ticket__flight']}>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>MOW – HKT</span>
-                <span className={classes['info__value']}>10:45 – 08:00</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>MOW – HKT</span>
-                <span className={classes['info__value']}>11:20 – 00:50</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>21ч 15м</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>13ч 30м</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>2 пересадки</span>
-                <span className={classes['info__value']}>HKG, JNB</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>1 пересадка</span>
-                <span className={classes['info__value']}>HKG</span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li className={classes.ticket}>
-          <div className={classes['ticket__header']}>
-            <span className={classes['ticket__price']}>13 400</span>
-            <div className={classes['ticket__logo']}>
-              <img src={s7Logo} alt="logo" />
-            </div>
-          </div>
-          <div className={classes['ticket__flight']}>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>MOW – HKT</span>
-                <span className={classes['info__value']}>10:45 – 08:00</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>MOW – HKT</span>
-                <span className={classes['info__value']}>11:20 – 00:50</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>21ч 15м</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>13ч 30м</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>2 пересадки</span>
-                <span className={classes['info__value']}>HKG, JNB</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>1 пересадка</span>
-                <span className={classes['info__value']}>HKG</span>
-              </div>
-            </div>
-          </div>
-        </li>
         <li className={classes.ticket}>
           <div className={classes['ticket__header']}>
             <span className={classes['ticket__price']}>13 400</span>
@@ -211,4 +56,23 @@ const Tickets = () => {
   );
 };
 
-export default Tickets;
+const mapStateToProps = (state) => {
+  return {
+    tickets: state.tickets,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadTickets: async () => {
+      const responseId = await fetch('https://aviasales-test-api.kata.academy/search');
+      const bodyId = await responseId.json();
+      const searchId = bodyId.searchId;
+      const responseTickets = await fetch(`https://aviasales-test-api.kata.academy/tickets?searchId=${searchId}`);
+      const bodyTickets = await responseTickets.json();
+      dispatch({ type: 'loadTickets', ticketsData: bodyTickets });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tickets);
--- src/reducer.js
+++ src/reducer.js
@@ -11,57 +11,84 @@ const initialState = {
     quickest: false,
     optimal: false,
   },
+
+  tickets: [
+    {
+      price: 24260,
+      carrier: 'W6',
+      segments: [
+        {
+          origin: 'MOW',
+          destination: 'HKT',
+          date: '2023-11-07T19:21:46.655Z',
+          duration: 554,
+          stops: [],
+        },
+        {
+          origin: 'HKT',
+          destination: 'MOW',
+          date: '2024-03-02T21:50:38.062Z',
+          duration: 841,
+          stops: ['DXB'],
+        },
+      ],
+    },
+  ],
 };
 
 const reducer = (state = initialState, action) => {
   const newState = JSON.parse(JSON.stringify(state));
-  const { transfers } = newState;
   const { type } = action;
+  const { transfers } = newState;
+
+  if (type === 'loadTickets') {
+    newState.tickets = action.ticketsData;
+    console.log(newState.tickets);
+    return newState;
+  }
 
   const checkAllBox = () => {
-    const { withoutTransfers, oneTransfer, twoTransfers, threeTransfers } =
-      transfers;
+    const { withoutTransfers, oneTransfer, twoTransfers, threeTransfers } = transfers;
     if (withoutTransfers && oneTransfer && twoTransfers && threeTransfers) {
       transfers.all = true;
     }
   };
 
-  if (type === "all" && transfers.all === false) {
+  if (type === 'all' && transfers.all === false) {
     for (let key in newState.transfers) transfers[key] = true;
     return newState;
-  } else if (type === "all" && transfers.all === true) {
+  } else if (type === 'all' && transfers.all === true) {
     for (let key in newState.transfers) transfers[key] = false;
     return newState;
   }
 
-  if (type === "withoutTransfers") {
+  if (type === 'withoutTransfers') {
     transfers.withoutTransfers = !transfers.withoutTransfers;
     if (transfers.all) transfers.all = false;
     checkAllBox();
     return newState;
   }
 
-  if (type === "oneTransfer") {
+  if (type === 'oneTransfer') {
     transfers.oneTransfer = !transfers.oneTransfer;
     if (transfers.all) transfers.all = false;
     checkAllBox();
     return newState;
   }
 
-  if (type === "twoTransfers") {
+  if (type === 'twoTransfers') {
     transfers.twoTransfers = !transfers.twoTransfers;
     if (transfers.all) transfers.all = false;
     checkAllBox();
     return newState;
   }
 
-  if (type === "threeTransfers") {
+  if (type === 'threeTransfers') {
     transfers.threeTransfers = !transfers.threeTransfers;
     if (transfers.all) transfers.all = false;
     checkAllBox();
     return newState;
   }
-
   return state;
 };
 

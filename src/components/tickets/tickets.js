d4a11065cb64082a7d749277e657f337217e43f8
gojimork <crusejohen@gmail.com>
2023-03-15 00:50:31 +0300

    replace createStore to configureStore

--- src/components/tickets/tickets.js
+++ src/components/tickets/tickets.js
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import classes from './tickets.module.scss';
-import { loadTickets } from '../../actions';
-import { Spin, Alert } from 'antd';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import classes from "./tickets.module.scss";
+import { loadTickets } from "../../actions";
+import { Spin, Alert } from "antd";
 
-const Tickets = ({ renderTickets, loadTickets, loader, onMoreTicketseClick, emptyList }) => {
-  console.log(renderTickets);
+const Tickets = ({
+  renderTickets,
+  loadTickets,
+  loader,
+  onMoreTicketseClick,
+  emptyList,
+}) => {
   useEffect(() => {
     loadTickets();
   }, [loadTickets]);
@@ -28,41 +33,47 @@ const Tickets = ({ renderTickets, loadTickets, loader, onMoreTicketseClick, empt
     }) => {
       return (
         <li key={id} className={classes.ticket}>
-          <div className={classes['ticket__header']}>
-            <span className={classes['ticket__price']}>{priceRender}</span>
-            <div className={classes['ticket__logo']}>
+          <div className={classes["ticket__header"]}>
+            <span className={classes["ticket__price"]}>{priceRender}</span>
+            <div className={classes["ticket__logo"]}>
               <img src={logoUrl} alt="logo" />
             </div>
           </div>
-          <div className={classes['ticket__flight']}>
-            <div className={classes['info-list']}>
+          <div className={classes["ticket__flight"]}>
+            <div className={classes["info-list"]}>
               <div className={classes.info}>
-                <span className={classes['info__title']}>{flight}</span>
-                <span className={classes['info__value']}>{time}</span>
+                <span className={classes["info__title"]}>{flight}</span>
+                <span className={classes["info__value"]}>{time}</span>
               </div>
               <div className={classes.info}>
-                <span className={classes['info__title']}>{flightBack}</span>
-                <span className={classes['info__value']}>{timeBack}</span>
-              </div>
-            </div>
-            <div className={classes['info-list']}>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>{durationRender}</span>
-              </div>
-              <div className={classes.info}>
-                <span className={classes['info__title']}>В пути</span>
-                <span className={classes['info__value']}>{durationRenderBack}</span>
+                <span className={classes["info__title"]}>{flightBack}</span>
+                <span className={classes["info__value"]}>{timeBack}</span>
               </div>
             </div>
-            <div className={classes['info-list']}>
+            <div className={classes["info-list"]}>
               <div className={classes.info}>
-                <span className={classes['info__title']}>{stopsCountRender}</span>
-                <span className={classes['info__value']}>{stopsList}</span>
+                <span className={classes["info__title"]}>В пути</span>
+                <span className={classes["info__value"]}>{durationRender}</span>
               </div>
               <div className={classes.info}>
-                <span className={classes['info__title']}>{stopsCountRenderBack}</span>
-                <span className={classes['info__value']}>{stopsListBack}</span>
+                <span className={classes["info__title"]}>В пути</span>
+                <span className={classes["info__value"]}>
+                  {durationRenderBack}
+                </span>
+              </div>
+            </div>
+            <div className={classes["info-list"]}>
+              <div className={classes.info}>
+                <span className={classes["info__title"]}>
+                  {stopsCountRender}
+                </span>
+                <span className={classes["info__value"]}>{stopsList}</span>
+              </div>
+              <div className={classes.info}>
+                <span className={classes["info__title"]}>
+                  {stopsCountRenderBack}
+                </span>
+                <span className={classes["info__value"]}>{stopsListBack}</span>
               </div>
             </div>
           </div>
@@ -71,19 +82,23 @@ const Tickets = ({ renderTickets, loadTickets, loader, onMoreTicketseClick, empt
     }
   );
   const btn = renderTickets.length ? (
-    <button onClick={onMoreTicketseClick} className={classes['more-tickets']}>
+    <button onClick={onMoreTicketseClick} className={classes["more-tickets"]}>
       Показать еще 5 билетов!
     </button>
   ) : null;
   const alert = emptyList ? (
-    <Alert message="Рейсов, подходящих под заданные фильтры, не найдено" type="info" showIcon />
+    <Alert
+      message="Рейсов, подходящих под заданные фильтры, не найдено"
+      type="info"
+      showIcon
+    />
   ) : null;
 
   return (
     <React.Fragment>
       <Spin spinning={loader} />
       {alert}
-      <ul className={classes['tickets-list']}>{ticketList}</ul>
+      <ul className={classes["tickets-list"]}>{ticketList}</ul>
       {btn}
     </React.Fragment>
   );
@@ -100,7 +115,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     loadTickets: loadTickets(dispatch),
-    onMoreTicketseClick: () => dispatch({ type: 'SHOW_MORE' }),
+    onMoreTicketseClick: () => dispatch({ type: "SHOW_MORE" }),
   };
 };
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/app';
-import reducer from './reducer';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app";
+import reducer from "./reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-const actionSanitizer = (action) =>
-  action.type === 'loadTickets' && action.ticketsData ? { ...action, ticketsData: '<<LONG_BLOB>>' } : action;
+const store = configureStore({
+  reducer,
+  devTools: true,
+  middleware: [thunk],
+});
 
-const enhancer = compose(
-  applyMiddleware(reduxThunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__({
-      actionSanitizer,
-      stateSanitizer: (state) => (state.ticketsData ? { ...state, ticketsData: '<<LONG_BLOB>>' } : state),
-    })
-);
-
-const store = createStore(reducer, enhancer);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
